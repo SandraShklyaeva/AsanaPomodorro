cfcfbe412d33904111799358d25ac6f0f1768526
Sandra Shklyaeva <2598116+SandraShklyaeva@users.noreply.github.com>
2019-08-09 20:55:57 +0300

    Added Github Pages
    Added GitHub Pages

---
+++ build/precache-manifest.f12806df55f9d716a7d3ba18219c4dd0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "826f08b4d7e66a893ab7c8430cbca8b9",
+    "url": "/AsanaPomodorro/index.html"
+  },
+  {
+    "revision": "f86ad66f2b7849ed23ae",
+    "url": "/AsanaPomodorro/static/css/main.686dbefa.chunk.css"
+  },
+  {
+    "revision": "8bb1cadb467b0a269773",
+    "url": "/AsanaPomodorro/static/js/2.283c56d7.chunk.js"
+  },
+  {
+    "revision": "f86ad66f2b7849ed23ae",
+    "url": "/AsanaPomodorro/static/js/main.14ab45e9.chunk.js"
+  },
+  {
+    "revision": "c6f504f78b3e4d24db2a",
+    "url": "/AsanaPomodorro/static/js/runtime~main.27dcb877.js"
+  }
+]);
